63a17ec74cc450edf2f099e4e423052a29b0405a
deeptanshu33 <Deeptanshu.me@outlook.com>
2024-07-19 21:01:25 +0530

    Initial commit

---
+++ Password_Man/urls.py
@@ -0,0 +1,31 @@
+"""
+URL configuration for Password_Man project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from passw.views import *
+
+urlpatterns = [
+    path("login/", login_page, name="login_page"),
+    path("logout/", logout_page, name="logout_page"),
+    path("register/", register, name="register"),
+    path("admin/", admin.site.urls),
+    path("saved/", saved_passwords, name='saved'),
+    path("add/", add_account, name='add'),
+    path("delete-acc/<id>/", delete_acc, name="delete-acc"),
+    path("update-acc/<id>/", update_acc, name="update-acc"),
+    path("", passw, name='passw'),
+]
---
+++ passw/views.py
@@ -0,0 +1,111 @@
+from django.shortcuts import render, redirect
+from .models import *
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout   #login maintains sessions
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+@login_required(login_url="/login/")
+def passw(request):
+    return render(request, 'index.html')
+
+def saved_passwords(request):
+    queryset = userAccount.objects.all()
+    
+    if request.GET.get('search'):
+        queryset = queryset.filter(name_service__icontains = request.GET.get('search'))
+
+    context = {'useraccs':queryset}
+    return render(request, 'saved.html', context)
+
+def add_account(request):
+    if request.method == "POST":
+        data = request.POST
+
+        name_service = data.get('name_service')
+        username_service = data.get('username_service')
+        password_service = data.get('password_service')
+
+        userAccount.objects.create(
+            name_service = name_service,
+            username_service = username_service,
+            password_service = password_service
+        )
+
+        return redirect('/add')
+    
+    return render(request, 'add.html')
+
+def delete_acc(request, id):
+    queryset = userAccount.objects.get(id=id)
+    queryset.delete()
+    return redirect('/saved')
+
+def update_acc(request, id):
+    queryset = userAccount.objects.get(id=id)
+    if request.method == 'POST':
+        data = request.POST
+        name_service = data.get('name_service')
+        username_service = data.get('username_service')
+        password_service = data.get('password_service')
+
+        queryset.name_service = name_service
+        queryset.username_service = username_service
+        queryset.password_service = password_service
+
+        queryset.save()
+
+        return redirect('/saved')
+
+    context = {'userAccs': queryset}
+
+    return render(request, 'update_acc.html', context)
+
+def login_page(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        if not User.objects.filter(username = username).exists():
+            messages.info(request, "Invalid Username")
+            return redirect("/login/")
+        
+        user = authenticate(username = username, password = password)
+        if user is None:
+            messages.info(request, "Invalid Password")
+            return redirect("/login/")
+        else:
+            login(request, user)
+            return redirect("/")
+        
+    return render(request, 'login_page.html')
+
+def logout_page(request):
+    logout(request)
+    return redirect("/login/")
+
+def register(request):
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = User.objects.filter(username = username)
+        if user.exists():
+            messages.info(request, "Username already exists.")
+            return redirect("/register/")
+
+        user = User.objects.create(
+            first_name = first_name,
+            last_name = last_name,
+            username = username,
+            password = password,
+        )
+
+        user.set_password(password) #predefined method in django to encrypt and save password
+        user.save()
+        # messages.info(request, "Account created successfully!")
+        return redirect('/login/')
+
+    return render(request, 'register.html')
